59dd08e2913a836ed5612db4e0474fd1527eb33f
Thomas Landspurg <thomas.landspurg@gmail.com>
2014-12-18 11:20:40 +0100

    Use env variable

--- tweet.js
+++ tweet.js
@@ -1,3 +1,8 @@
+// @ThomasLandspurg 2014
+// Node.js client to search all tweet within a geographical zone and forwared them
+// in real time using a socket
+// http://blog.landspurg.net
+//
 var express = require('express'),
 app = express(),
 http = require('http'),
@@ -13,16 +18,15 @@ app.get('/', function (req, res) {
 });
 app.use(express.static(__dirname + '/public'));
 
-var world = [ '-180', '-90', '180', '90' ]
-var currentBounds=world;
+var world = [ '-180', '-90', '180', '90' ];
 var total=0;
 var totalSent=0;
 
-var T = new Twit({
-  consumer_key:         'OyBoq9N9n2AmCqkVLQ8PpKor8',
-  consumer_secret:      'StmNJ3xlnKvLitgLDqq36Q5wr0RsGAJ5FDZI8W9Yul5sZpaI9a',
-  access_token:         '2933552610-wji03WrMTSPG9rCP49n8ygem9ibUds3oUjLDBra',
-  access_token_secret:  '0EBgwNQTYGLOVteqzkl0RPiVflpNPfhSKgBBNPAgfG3JE'
+var T = new Twit({  // You need to setup your own twitter configuration here!
+  consumer_key:    process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token:    process.env.ACCESS_TOKEN,
+  access_token_secret:process.env.ACCESS_TOKEN_SECRET
 });
 
 var stream = T.stream('statuses/filter', { locations: world});
@@ -39,33 +43,32 @@ stream.on('tweet', function (tweet) {
     clients.forEach(function(socket){
       var currentBounds=bounds_for_socket[socket.id];
 
-      if(currentBounds&&(coords[1]>currentBounds[0])&&(coords[0]>currentBounds[1])&&(coords[1]<currentBounds[2])&&(coords[0]<currentBounds[3])){
-        //console.log(coords+" "+currentBounds);
-        //console.log("...in");
+      if(currentBounds&&(coords[1]>currentBounds[0])&&(coords[0]>currentBounds[1])
+                      &&(coords[1]<currentBounds[2])&&(coords[0]<currentBounds[3])){
+
         totalSent+=1;
         if(totalSent%100==0)console.log("Sent:"+totalSent);
         var smallTweet={
           text:tweet.text,
-          user:{screen_name:tweet.user.screen_name,profile_image_url:tweet.user.profile_image_url,id_str:tweet.user.id_str},
+          user:{   screen_name:       tweet.user.screen_name,
+                   profile_image_url: tweet.user.profile_image_url,
+                   id_str:            tweet.user.id_str},
           geo:tweet.geo
         };
-       // console.log(smallTweet);
         socket.emit('stream',smallTweet);
       }
     });
   }
   });
 
-var bounds_for_socket={};
-var clients=[];
+var bounds_for_socket={}; // Will contains a hash association between socket_id -> map bound for this client
+var clients=[];  // the list of connected clients
 io.sockets.on('connection', function (socket) {
   socket.on('recenter',function(msg){
     console.log("recenter:"+msg);
     bounds_for_socket[this.id]=JSON.parse("["+msg+"]");
-    console.log(bounds_for_socket);
   });
   socket.on('disconnect',function(socket){
-    console.log("DISCONNET:"+this.id);
     //  Here we try to get the correct element in the client list
     for(var i=0;i<clients.length;i++){
       client=clients[i];
@@ -75,7 +78,6 @@ io.sockets.on('connection', function (socket) {
 
     console.log("disconnect , there is still:"+clients.length+" connected ("+Object.keys(bounds_for_socket).length+')');
   });
-  console.log("NEW ID:"+socket.id);
   clients.push(socket); // Update the list of connected clients
   currentBounds=JSON.parse(socket.handshake.query.bounds);
   bounds_for_socket[socket.id]=currentBounds;
