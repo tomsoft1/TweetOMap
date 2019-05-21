0d90304bc1020bdaf1079c8e729148ab83c4848d
Thomas Landspurg <thomas.landspurg@gmail.com>
2019-05-21 10:35:03 +0200

    Port is now in config

--- tweet.js
+++ tweet.js
@@ -4,7 +4,7 @@
 // in real time using a socket
 // http://blog.landspurg.net
 //
-//require('dotenv').config();
+require('dotenv').config();
 var express = require('express'),
 app = express(),
 http = require('http'),
@@ -12,7 +12,7 @@ server = http.createServer(app),
 Twit = require('twit'),
 io = require('socket.io').listen(server);
 
-server.listen(8080);
+server.listen( process.env.PORT||8080);
 
 // routing
 app.get('/', function (req, res) {
